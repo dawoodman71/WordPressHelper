8305579766fe70991fa9a749610a2b0715ef500b
John Woodcock <john@userx.co>
2019-10-06 01:19:20 -0400

    First commit

---
+++ src/WordPressHelper/WordPressHelper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jwoodcock
+ * Date: 10/6/19
+ * Time: 1:00 AM
+ */
+
+namespace WordPressHelper;
+
+
+class WordPressHelper
+{
+
+    public static function test(){
+        return "This is a test.";
+    }
+
+}
